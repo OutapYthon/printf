7ca766e2d57024e9c67aa0dcb2fdab0312545399
HOUDA <129116855+hudalaa@users.noreply.github.com>
2023-08-23 01:27:51 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,74 +1,90 @@
-#include <stdarg.h>
-#include <unistd.h>
 #include "main.h"
 
+void print_buffer(char buffer[], int *buff_ind);
+
 /**
- * _printf - Custom printf function
- * @format: The format string
- * @...: The variable arguments
- *
- * Return: The number of characters printed
+ * _printf - Printf function
+ * @format: format.
+ * Return: Printed chars.
  */
 int _printf(const char *format, ...)
 {
-    va_list args;
-    int count = 0;
+    int i, printed = 0, printed_chars = 0;
+    int buff_ind = 0;
+    va_list list;
+    char buffer[BUFF_SIZE];
 
-    va_start(args, format);
+    if (format == NULL)
+        return (-1);
 
-    while (*format)
+    va_start(list, format);
+
+    for (i = 0; format && format[i] != '\0'; i++)
     {
-        if (*format == '%')
+        if (format[i] != '%')
         {
-            format++;
-            if (*format == '\0')
-                break;
-            if (*format == 'c')
-            {
-                char c = va_arg(args, int);
-                write(1, &c, 1);
-                count++;
-            }
-            else if (*format == 's')
-            {
-                char *str = va_arg(args, char *);
-                if (str == NULL)
-                    str = "(null)";
-                while (*str)
-                {
-                    write(1, str, 1);
-                    str++;
-                    count++;
-                }
-            }
-            else if (*format == '%')
-            {
-                write(1, "%", 1);
-                count++;
-            }
-            else if (*format == 'd' || *format == 'i')
-            {
-                int num = va_arg(args, int);
-                char num_str[12]; // Assuming maximum integer length is 11 digits
-                snprintf(num_str, sizeof(num_str), "%d", num);
-                int len = 0;
-                while (num_str[len] != '\0')
-                {
-                    write(1, &num_str[len], 1);
-                    len++;
-                    count++;
-                }
-            }
+            buffer[buff_ind++] = format[i];
+            if (buff_ind == BUFF_SIZE)
+                print_buffer(buffer, &buff_ind);
+            printed_chars++;
         }
         else
         {
-            write(1, format, 1);
-            count++;
+            print_buffer(buffer, &buff_ind);
+            ++i;
+            printed = handle_print(format, &i, list, buffer);
+            if (printed == -1)
+                return (-1);
+            printed_chars += printed;
         }
-        format++;
     }
 
-    va_end(args);
+    print_buffer(buffer, &buff_ind);
 
-    return count;
+    va_end(list);
+
+    return (printed_chars);
+}
+
+/**
+ * print_buffer - Prints the contents of the buffer if it exists
+ * @buffer: Array of chars
+ * @buff_ind: Index at which to add next char, represents the length.
+ */
+void print_buffer(char buffer[], int *buff_ind)
+{
+    if (*buff_ind > 0)
+        write(1, &buffer[0], *buff_ind);
+
+    *buff_ind = 0;
+}
+
+/**
+ * handle_print - Handles format specifiers and prints accordingly
+ * @format: The format string
+ * @i: Pointer to the current position in the format string
+ * @list: The variable argument list
+ * @buffer: Buffer to store characters
+ * Return: Number of characters printed
+ */
+int handle_print(const char *format, int *i, va_list list, char buffer[])
+{
+    // Implementation for handling %d and %i format specifiers
+    if (format[*i] == 'd' || format[*i] == 'i')
+    {
+        int num = va_arg(list, int);
+        char num_str[12]; // Assuming maximum integer length is 11 digits
+        snprintf(num_str, sizeof(num_str), "%d", num);
+        int len = 0;
+        while (num_str[len] != '\0')
+        {
+            buffer[*i] = num_str[len++];
+            (*i)++;
+        }
+        return len;
+    }
+    else
+    {
+        // Handle other format specifiers (e.g., %c, %s, %%, etc.) as before
+    }
 }
